5584d92b93e74436b01f6aa0fd44779de85fe4fa
Dalian Grullon <dalian.business@aol.com>
2022-08-18 21:37:48 -0400

    Created thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,19 @@
 const router = require('express').Router();
 
+// /api/thoughts
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
+
+router.route('/:thoughtId')
+  .get(getThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+  .post(createReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
+
 module.exports = router;
