1bdb7aeb675404bfe21480a43768c42a6d5845e9
Dalian Grullon <dalian.business@aol.com>
2022-08-18 21:30:06 -0400

    Created all userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id
+router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
 module.exports = router;
