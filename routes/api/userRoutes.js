ae6a61cf5002870b44706bffefa3b7565043c694
Dalian Grullon <dalian.business@aol.com>
2022-08-18 21:31:42 -0400

    Removed unnecessary commenting from userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
-// /api/users/:id
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
 
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
